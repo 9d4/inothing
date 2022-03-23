0719907719291b25cd05c7230c30de0313b2aa24
BayuDC <dev.bayudc@gmail.com>
2022-03-23 23:49:14 +0700

    create basic vue struct

---
+++ src/components/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "App",
+    data() {
+        return {};
+    },
+};
+</script>
+
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp, ref } from "vue";
+import App from "./components/App.vue";
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    createApp(App).mount(createAppDiv());
+}
+
+function createAppDiv() {
+    // create div#app
+    const appEl = document.createElement("div");
+    appEl.id = "#app";
+
+    document.body.appendChild(appEl);
+    return appEl;
+}
